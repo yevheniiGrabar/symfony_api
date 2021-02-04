6bd15b86a99f762a5a60b4a9e2dcc781302b3ea9
stas_lampdev <lampdev.dev7@gmail.com>
2021-02-04 21:14:59 +0200

    SA-3 * Added StoreAction fix action name

--- symfony_backend/src/Controller/PostsController.php
+++ symfony_backend/src/Controller/PostsController.php
@@ -3,16 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\User;
 use App\Repository\PostRepository;
 use App\Repository\UserRepository;
 use App\Services\PostRequestParser;
 use App\Services\UserRequestParser;
 use App\Services\UserRequestValidator;
+use Carbon\Carbon;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @Route("api/posts", name="posts.")
@@ -40,31 +45,46 @@ class PostsController extends AbstractController implements TokenAuthenticatedCo
         $this->postRequestParser = $postRequestParser;
     }
 
-//    /**
-//     * @Route("/store", name="store", methods={"POST"})
-//     * @param Request $request
-//     * @return JsonResponse
-//     */
-//    public function storePost(Request $request): JsonResponse
-//    {
-//
-//        $request = $this->postRequestParser->PostParseRequest($request);
-//
-//        $post = new Post();
-//        $post->setTitle($request->title);
-//        $post->setContent($request->content);
-//        $post->setCreatedAt();
-//        $this->postRepository->plush($post);
-//
-//        return new JsonResponse($post->getPostData());
-//    }
+    /**
+     * @Route("/store", name="store", methods={"POST"})
+     * @param Request $request
+     * @param UserInterface $userInterface
+     * @return JsonResponse
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function storeAction(Request $request, UserInterface $userInterface): JsonResponse
+    {
+        $userId = $userInterface->getId();
+        $user = $this->userRepository->findOneBy(['id' => $userId]);
+
+        if(!$user) {
+            return new JsonResponse(UserRequestValidator::USER_NOT_FOUND_MESSAGE, Response::HTTP_NOT_FOUND);
+        }
+
+        if ($userId != $user->id) {
+            return new JsonResponse(UserRequestValidator::ACCESS_DENIED_MESSAGE, Response::HTTP_FORBIDDEN);
+        }
+
+        $postDate = $this->postRepository->find('createdAt');
+        $postDate = Carbon::now($postDate);
+        $post = new Post();
+        $post->setTitle($request->get('title', ''));
+        $post->setContent($request->get('content', ''));
+        $post->setCreatedAt(Carbon::now());
+        $post->setUpdatedAt($postDate);
+        $post->setUser($user);
+        $this->postRepository->plush($post);
+
+        return new JsonResponse($post->getPostData());
+    }
 
     /**
      * @Route("/show/{id}", name="show", methods={"GET"})
      * @param int $id
      * @return JsonResponse
      */
-    public function showPost(int $id): JsonResponse
+    public function showAction(int $id): JsonResponse
     {
         $post = $this->postRepository->find($id);
 
@@ -78,14 +98,14 @@ class PostsController extends AbstractController implements TokenAuthenticatedCo
     /**
      * * @Route("/update/{id}", name="update", methods={"PUT"})
      */
-    public function updatePost(): JsonResponse
+    public function updateAction(): JsonResponse
     {
     }
 
     /**
      * * @Route("/delete/{id}", name="delete", methods={"DELETE"})
      */
-    public function deletePost()
+    public function deleteAction()
     {
         dd(4);
     }
