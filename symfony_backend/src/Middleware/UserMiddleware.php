ec2fb205c238539edf76aa75575b7603da6d95a3
stas_lampdev <lampdev.dev7@gmail.com>
2021-02-05 13:07:41 +0200

    SA-3 * Rework UserMiddleware
    Added new userEndpoint for api/posts/show added new errors Excaptions

--- symfony_backend/src/Middleware/UserMiddleware.php
+++ symfony_backend/src/Middleware/UserMiddleware.php
@@ -2,9 +2,13 @@
 
 namespace App\Middleware;
 
+use App\Entity\Post;
 use App\Entity\User;
+use App\Repository\PostRepository;
 use App\Services\RolesManager;
 use App\Services\UserRequestParser;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\Response;
 use App\Controller\TokenAuthenticatedController;
@@ -15,6 +19,10 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
+/**
+ * Class UserMiddleware
+ * @package App\Middleware
+ */
 class UserMiddleware implements EventSubscriberInterface
 {
     private UrlGeneratorInterface $router;
@@ -28,21 +36,30 @@ class UserMiddleware implements EventSubscriberInterface
     /** @var RolesManager */
     private RolesManager $rolesManager;
 
+    /** @var PostRepository */
+    private PostRepository $postRepository;
+
     public function __construct
     (
         UrlGeneratorInterface $router,
         UserRequestParser $userRequestParser,
         TokenStorageInterface $storage,
-        RolesManager $rolesManager
+        RolesManager $rolesManager,
+        PostRepository $postRepository
     )
     {
         $this->router = $router;
         $this->userRequestParser = $userRequestParser;
         $this->tokenStorageInterface = $storage;
         $this->rolesManager = $rolesManager;
+        $this->postRepository = $postRepository;
     }
 
-    /** @param ControllerEvent $event */
+    /**
+     * @param ControllerEvent $event
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
     public function onKernelController(ControllerEvent $event): void
     {
         /** @var AbstractController|AbstractController[] $controller */
@@ -58,16 +75,25 @@ class UserMiddleware implements EventSubscriberInterface
 
         $request = $event->getRequest();
         $user = $this->tokenStorageInterface->getToken()->getUser();
+        $post = $this->postRepository->find(['id' => $request->get('id')]);
 
         if (!$user instanceof User) {
             throw new HttpException(Response::HTTP_UNAUTHORIZED, 'Unauthorized');
+        }
+
+        if (!$post instanceof Post) {
+            throw new HttpException(Response::HTTP_NOT_FOUND, 'Post not found');
+        }
+
+        if ($post->getUser()->getId() != $user->getId()) {
+            throw new HttpException(Response::HTTP_UNAUTHORIZED, 'Access denied');
         }
 
         if ($user->isAdmin) {
             return;
         }
 
-        $defaultUserEndpoints = $this->userEndpoints($user->getId());
+        $defaultUserEndpoints = $this->userEndpoints($user->getId(), $post->getId());
         $requestUri = $request->getRequestUri();
 
         if (!in_array($requestUri, $defaultUserEndpoints)) {
@@ -88,14 +114,16 @@ class UserMiddleware implements EventSubscriberInterface
 
     /**
      * @param int $userId
+     * @param int $postId
      * @return array
      */
-    public function userEndpoints(int $userId): array
+    public function userEndpoints(int $userId, int $postId): array
     {
         return [
             $this->router->generate('users.show', ['id' => $userId]),
             $this->router->generate('users.update', ['id' => $userId]),
-            $this->router->generate('users.delete', ['id' => $userId])
+            $this->router->generate('users.delete', ['id' => $userId]),
+            $this->router->generate('posts.show', ['id' => $postId])
         ];
     }
 }
